38d39c7d44b6144f74e5f6beacf084d78ef54537
MoffuGit <moffu.by@gmail.com>
2025-09-13 18:31:18 -0600

    EnterRawMode

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,41 @@
 const std = @import("std");
-const kilo = @import("kilo");
+const posix = std.posix;
+const debug = std.debug;
+const ascii = std.ascii;
+const fs = std.fs;
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try kilo.bufferedPrint();
-}
+    const tty_file = try fs.openFileAbsolute("/dev/tty", .{});
+    defer tty_file.close();
+    const tty_fd = tty_file.handle;
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    const old_settings = try posix.tcgetattr(tty_fd);
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    var new_settings = old_settings;
+    new_settings.lflag.ECHO = false;
+    new_settings.lflag.ICANON = false;
+
+    _ = try posix.tcsetattr(tty_fd, posix.TCSA.NOW, new_settings);
+
+    debug.print("--- starting input --- (press 'q' to quit)\n", .{});
+
+    var buffer: [1]u8 = undefined;
+    while (true and buffer[0] != 'q') {
+        _ = try tty_file.read(&buffer);
+
+        if (ascii.isControl(buffer[0])) {
+            debug.print("input: {}\r\n", .{
+                buffer[0],
+            });
+        } else {
+            debug.print("input: '{c}' ({d})\r\n", .{
+                .c = buffer[0],
+                .d = buffer[0],
+            });
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    }
+
+    _ = try posix.tcsetattr(tty_fd, posix.TCSA.NOW, old_settings);
+
+    debug.print("--- ending --- \n", .{});
 }
