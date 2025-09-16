6bd64218f058877dbfe354a44113efae97a9a89e
MoffuGit <moffu.by@gmail.com>
2025-09-16 11:07:57 -0600

    Open and read file

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,37 @@
 const Editor = @import("editor");
+const std = @import("std");
 
 pub fn main() !void {
-    var editor = Editor.init();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+    var editor = try Editor.init(alloc);
+    defer editor.deinit();
+    var args = std.process.args();
+    _ = args.next();
+    if (args.next()) |filename_bytes| {
+        const filename: []const u8 = filename_bytes;
+
+        var file: std.fs.File = undefined;
+        var file_opened_or_created = false;
+
+        const open_result = std.fs.cwd().openFile(filename, .{ .mode = .read_write });
+
+        if (open_result) |f| {
+            file = f;
+            file_opened_or_created = true;
+        } else |err| switch (err) {
+            error.FileNotFound => {
+                file = try std.fs.cwd().createFile(filename, .{});
+                file_opened_or_created = true;
+            },
+            else => return err,
+        }
+
+        if (file_opened_or_created) {
+            defer file.close();
+            try editor.open(file);
+        }
+    }
     try editor.run();
 }
